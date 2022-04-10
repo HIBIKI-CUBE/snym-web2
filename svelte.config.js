825ea18d16fe8139cc61416087f18b22592d1d86
HIBIKI <hibiki.apple.1984@gmail.com>
2022-04-10 21:23:26 +0900

    Netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,15 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
-
-/** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   // Consult https://github.com/sveltejs/svelte-preprocess
-//   // for more information about preprocessors
-//   preprocess: preprocess(),
-//   kit: {
-//     adapter: adapter()
-//   }
-// };
-
-const dev = process.env.NODE_ENV === 'development';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
   preprocess: preprocess(),
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : '/snym-web2'
-    },
-    // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-    // For example, instead of '_app', use 'app_', 'internal', etc.
-    appDir: 'internal'
+    adapter: adapter({
+      split: false
+    })
   }
 };
 
