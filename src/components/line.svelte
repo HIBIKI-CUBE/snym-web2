f2edccded0654043a13bbd70525ad298d39099da
HIBIKI <hibiki.apple.1984@gmail.com>
2022-04-21 14:51:57 +0900

    Dev Support markdown style link on Line component

--- src/components/line.svelte
+++ src/components/line.svelte
@@ -1,23 +1,87 @@
 <script lang="ts">
-  /** 半角スペースで分割した文字列 */
-  export let string = '';
+  /** 半角スペースで分割した文字列(スペース２つで通常のスペースを表示) */
+  export let content: string = '',
+    mdMode = false,
+    noLine = false;
+
+  function processMd(input: string) {
+    let result: string[] = [input];
+    if(!input.match(/\[.+?\]\(.+?\)/g)){
+      return result;
+    }
+    input.match(/\[.+?\]\(.+?\)/g).forEach((v) => {
+      const tmp = result.pop().split(v);
+      tmp.splice(1, 0, v);
+      result = result.concat(tmp);
+    });
+    return result;
+  }
+
+  function splitDualSpace(input: string) {
+    return input
+      .replaceAll('  ', '\t ')
+      .split(' ')
+      .map((v) => v.replaceAll('\t', ' '));
+  }
 </script>
 
-{#if string.includes('\n')}
-  {#each string.split('\n') as line}
+{#if mdMode}
+  {#if content.includes('\n')}
+    {#each content.split('\n') as line}
+      <div>
+        {#each processMd(line) as rawChunk}
+          {#if rawChunk.match(/\[.+?\]\(.+?\)/)}
+            <a href={rawChunk.match(/\((.*?)\)/)[1]}>
+              {#each splitDualSpace(rawChunk.match(/\[(.*?)\]/)[1]) as chunk}
+                <span class="chunk {noLine && 'noLine'}">{chunk}</span>
+              {/each}
+            </a>
+          {:else}
+            {#each splitDualSpace(rawChunk) as chunk}
+              <span class="chunk {noLine && 'noLine'}">{chunk}</span>
+            {/each}
+          {/if}
+        {/each}
+      </div>
+    {/each}
+  {:else}
+    {#each processMd(content) as rawChunk}
+      {#if rawChunk.match(/\[.+?\]\(.+?\)/)}
+        <a href={rawChunk.match(/\((.*?)\)/)[1]}>
+          {#each splitDualSpace(rawChunk.match(/\[(.*?)\]/)[1]) as chunk}
+            <span class="chunk {noLine && 'noLine'}">{chunk}</span>
+          {/each}
+        </a>
+      {:else}
+        {#each splitDualSpace(rawChunk) as chunk}
+          <span class="chunk {noLine && 'noLine'}">{chunk}</span>
+        {/each}
+      {/if}
+    {/each}
+  {/if}
+{:else if content.includes('\n')}
+  {#each content.split('\n') as line}
     <div>
-      {#each line.split(' ') as chunk}
-        <span>{chunk}</span>
+      {#each splitDualSpace(line) as chunk}
+        <span class="chunk {noLine && 'noLine'}">{chunk}</span>
       {/each}
     </div>
   {/each}
 {:else}
-  {#each string.split(' ') as chunk}
-    <span>{chunk}</span>
+  {#each splitDualSpace(content) as chunk}
+    <span class="chunk {noLine && 'noLine'}">{chunk}</span>
   {/each}
 {/if}
 
 <style lang="stylus">
-  span
+  a
+    text-decoration: none
+    white-space: normal!important
+
+  .chunk
     display: inline-block
+    white-space: pre-wrap
+
+    :global(a) &:not(.noLine)
+      text-decoration: underline
 </style>
